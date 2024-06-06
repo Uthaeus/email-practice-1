ae23273a71681bd8ed26c6e8d4e9fccc9c40af8a
Uthaeus <romanlavery@gmail.com>
2024-06-06 16:11:53 -0600

    created root layout, home, and contact components, initialized routes in app

--- src/App.js
+++ src/App.js
@@ -1,3 +1,25 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import RootLayout from "./components/layouts/root-layout";
+import Home from "./pages/home";
+import Contact from "./pages/contact";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
